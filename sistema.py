0fd4bfa95c15f0d0d5528e08c2e786640a89b3bc
Diego Silva <diegosilvacba2017@gmail.com>
2024-05-08 10:47:20 -0300

    Adiciona automação de abertura do navegador
    Integração de automação com PyAutoGUI

---
+++ sistema.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+pyautogui.click  # clicar
+pyautogui.write  # escrever um texto
+pyautogui.press  # precionar 
+
+# abrindo navegador
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
