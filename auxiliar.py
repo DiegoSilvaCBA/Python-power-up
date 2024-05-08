8b0bd6e3b1d613aa542b91461adcb20ba7f0dd37
Diego Silva <diegosilvacba2017@gmail.com>
2024-05-08 11:28:46 -0300

    Descobrindo o valor do local em específico

---
+++ auxiliar.py
@@ -0,0 +1,5 @@
+import pyautogui
+import time
+
+time.sleep(5.0) # tempo de colocar o mouse no lugar da tela que vai ser executado o click
+print(pyautogui.position()) # mostra o valor do ponto específico do click, agora só add no outro arquivo
