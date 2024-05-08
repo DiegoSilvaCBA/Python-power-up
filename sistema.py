f1adfe8783357f985128172174164b909f69acb8
Diego Silva <diegosilvacba2017@gmail.com>
2024-05-08 11:04:12 -0300

    Entrando no site do sistema

--- sistema.py
+++ sistema.py
@@ -1,10 +1,16 @@
 import pyautogui
 
-pyautogui.click  # clicar
-pyautogui.write  # escrever um texto
-pyautogui.press  # precionar 
+pyautogui.PAUSE = 0.8 # pausa para execução de cada comando
 
-# abrindo navegador
+pyautogui.click  # click: para clicar em uma posição na tela
+pyautogui.write  # write: para escrever um texto
+pyautogui.press  # press: para pressionar uma tecla
+
+# abrir navegador
 pyautogui.press("win")
 pyautogui.write("chrome")
+pyautogui.press("enter")
+
+# entrar no sitema
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
